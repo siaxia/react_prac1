35b92cf19f9a9fe262805ff0791be0f72d0fb36c
Lee Chanbin <nyamnyam0325@naver.com>
2021-01-11 11:22:40 +0900

    ver 0.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li>hi</li>
+          <li>hello</li>
+          <li>bye</li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    )
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="hi" sub="hello!"></Subject>
+        <TOC></TOC>
+        <Content title="hi" desc="hi means hi!"></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
