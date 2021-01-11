00cd6ed140e6ff6b8259c02446a84965b992d2ae
Lee Chanbin <nyamnyam0325@naver.com>
2021-01-11 12:09:18 +0900

    ver 0.3

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import React, {Component} from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import logo from "./logo.svg";
 import "./App.css";
 
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li>hi</li>
-          <li>hello</li>
-          <li>bye</li>
-        </ul>
-      </nav>
-    )
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    )
-  }
-}
 class App extends Component {
   render() {
     return (
---
+++ src/components/TOC.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>hi</li>
+          <li>hello</li>
+          <li>bye</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC;
